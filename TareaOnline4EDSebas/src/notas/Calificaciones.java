805d00b0f6f962082a14d843af4620dcd59871d0
sebas <sebas@DESKTOP-K609059>
2023-02-26 19:15:59 +0100

    4.Utiliza las funciones de refactorización para sustituir el código que se encarga de especificar la calificación en función de la nota por un método que llamarás "cali", que será público, de forma que se incluyan los comentarios para la documentación si ello es posible.

--- TareaOnline4EDSebas/src/notas/Calificaciones.java
+++ TareaOnline4EDSebas/src/notas/Calificaciones.java
@@ -5,24 +5,26 @@ import java.util.Scanner;
 public class Calificaciones {
 
 public static void main(String[] args) {
-Scanner Entrada = new Scanner(System.in);// Para pedir un dato numérico por teclado
-int n = 0;
-String c = "";
-System.out.print("Introduzca el valor de una calificación: ");
-n = Entrada.nextInt();
 
-if (n >= 0 && n < 5)
-c = "Suspenso";
-else if (n >= 5 && n < 7)
-c = "Aprobado";
-else if (n >= 7 && n <9)
-c = "Notable";
-else if (n >= 9 && n <= 10)
-c = "Sobresaliente";
-else
-c = "El valor de la calificación introducida no es correcta";
-
-System.out.println(c);
+final Scanner Entrada = new Scanner(System.in); 
+System.out.println("Introduzca el valor de una calificación: "); 
+int n = Entrada.nextInt(); 
 Entrada.close();
+cali(n); 
+}
+
+public static void cali(int n) {
+	String c;
+	if (n >= 0 && n< 5) {	c  = "Suspenso";}
+
+	else if (n >= 5 && n< 7){	c  = "Aprobado";}
+
+	else if (n >= 7 && n <9){	c  = "Notable";}
+
+	else if (n >= 9 && n<= 10){	c  = "Sobresaliente";}
+
+	else {c  = "El valor de la calificación introducida no es correcta";}
+
+	System.out.println(c );
 }
 }
